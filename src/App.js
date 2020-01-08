6be14abfbb28abbcd787c698d58d2afc875cded8
Atul Kumar <Atul.Kumar3@go-mmt.com>
2020-01-08 18:54:34 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ function App() {
     <Fragment className="App">
       
       <div className="person">
-        <h1>Atul</h1>
+        <h1>Atul kumar</h1>
         <p>My age : 29</p>
       </div>
 
       <Person name="amit" age="49"/>
-
     </Fragment>
   );
 }
