bce6c58cde2c198ae3efa479f3b4ab3fa3798bdf
Atul Kumar <Atul.Kumar3@go-mmt.com>
2020-01-08 20:25:27 +0530

    state and props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Fragment } from 'react';
+import React, {  Component } from 'react';
 import './App.css';
-import Person from "./component/Person"
+import Person from "./Component/Person"
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'max', age: 28},
+      {name: 'manu', age: 29},
+      {name: 'atul', age: 27}
+    ], 
+    otherState: "Other state"
+  }
+
+   swithcNameHandler = () => {
+    this.setState({persons: [
+      {name: 'Rohan', age: 28},
+      {name: 'Baze', age: 29},
+      {name: 'Rahul', age: 27}
+    ]})
+  
+  }
+
+  render() {
+    return (
     <div className="App">    
-      <div className="person">
-        <h1>Atul kumar</h1>
-        <p>My age : 29</p>
-      </div>
-      <Person name="amit" age="49"/>
+      <button onClick={this.swithcNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby : Cricket </Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     // React.createElement('div', null, React.createElement('h1', null, 'This works now'))
   );
+}
 }
 
 export default App;
--- src/component/Person.js
+++ src/component/Person.js
@@ -3,8 +3,9 @@ import React from 'react'
 export const Person = (props) => {
   return (
     <div className="person">
-<h1>{props.name}</h1>
-  <p>Age : {props.age}</p>
+      <h1> {props.name}</h1>
+      <p>Age : {props.age}</p>
+      <p>{props.children}</p>    
     </div>
   )
 }
