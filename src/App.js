84c066ebb76fe56c4a6eaf131dc414c34df4fd83
Atul Kumar <Atul.Kumar3@go-mmt.com>
2020-01-08 19:07:16 +0530

    Jsx

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Person from "./component/Person"
 
 function App() {
   return (
-    <Fragment className="App">
-      
+    <div className="App">    
       <div className="person">
         <h1>Atul kumar</h1>
         <p>My age : 29</p>
       </div>
-
       <Person name="amit" age="49"/>
-    </Fragment>
+    </div>
+    // React.createElement('div', null, React.createElement('h1', null, 'This works now'))
   );
 }
 
